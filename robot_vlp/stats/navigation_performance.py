ba57bf23b2a406f5d21de677491e45f1bf9b15b6
Tyrel Glass <tyrel.glass@protag.co.nz>
2025-05-23 18:57:11 +1200

    exploration of vlp accuracy

--- robot_vlp/stats/navigation_performance.py
+++ robot_vlp/stats/navigation_performance.py
@@ -7,3 +7,8 @@ def calc_loc_err(pre, true):
     y_d = pre[:,1] - true[:,1]
     errs = np.sqrt(np.square(x_d) + np.square(y_d))
     return errs
+
+
+def normalize_angle_rad(angle):
+    """Normalize an angle in radians to the range [-π, π]."""
+    return (angle + np.pi) % (2 * np.pi) - np.pi
